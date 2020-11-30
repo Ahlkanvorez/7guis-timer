6845c6be88dc06f3f09481be3583611746c00b72
Robert Mitchell <robert.mitchell36@gmail.com>
2020-11-30 16:29:26 -0600

    Add initial implementation
    - Dynamic updates handled by mutual calling of setTimeout & rendering;
      the timeout changes the state on an interval, resulting in a render,
      and thus the creation of a new timeout on the same interval
    - Progress bar is implemented as a colored div in a flexbox
    - User driven single source of truth for UI state

--- src/timer/core.cljs
+++ src/timer/core.cljs
@@ -2,6 +2,49 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rd]))
 
+(defn time-since [start]
+  (/ (- (js/Date.now) start) 1000.0))
+
+(defn progress-bar [progress total]
+  [:div {:style {:background-color :white
+                 :border "1px solid gray"
+                 :flex "1 0"
+                 :display :flex}}
+   [:div {:style {:background-color "#89cff0"
+                  :width (str (* 100 (/ progress total)) "%")}}]])
+
+(defn bar-timer [progress total]
+  [:div {:style {:display :flex :justify-content :space-between}}
+   [:span {:style {:margin-right "10px"}} "Elapsed Time:"]
+   [progress-bar progress total]])
+
+(defn numeric-timer [elapsed] [:span (str (int elapsed) "s")])
+
+(defn duration-slider [duration]
+  [:div {:style {:display :flex :justify-content :space-between}}
+   [:span {:style {:margin-right "10px"}} "Duration: "]
+   [:input {:type :range
+            :min 1 :max 100 :step 1
+            :value @duration
+            :on-change #(reset! duration (.. % -target -value))}]])
+
+(defn timer []
+  (let [elapsed (r/atom 0)
+        started (r/atom (js/Date.now))
+        duration (r/atom 10)]
+    (fn []
+      (when (< @elapsed @duration)
+        (js/setTimeout #(reset! elapsed (time-since @started)) 10))
+      [:div {:style {:display :flex :justify-content :center}}
+       [:div {:style {:display :flex :flex-direction :column}}
+        [bar-timer @elapsed @duration]
+        [numeric-timer @elapsed]
+        [duration-slider duration]
+        [:input {:type :button
+                 :value :Reset
+                 :on-click #(do (reset! started (js/Date.now))
+                                (reset! elapsed 0))}]]])))
+
 (defn mount-root []
-  (rd/render [:h1 "Timer"]
+  (rd/render [timer]
              (js/document.getElementById "app-root")))
